148cadbcff75a03a7590489329339b14e6167b7f
Tanju Baştürk <basturktanju@gmail.com>
2026-02-18 02:11:51 +0300

    Fix port to 3001

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
     exec_mode: 'fork',
     env: {
       NODE_ENV: 'production',
-      PORT: 3000,
+      PORT: 3001,
       HOSTNAME: '0.0.0.0'
     },
     error_file: './logs/err.log',
