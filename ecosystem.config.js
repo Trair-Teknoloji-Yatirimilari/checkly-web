86c7e8f34d4054dc49d7ae90a5a2d6418cc4088e
Tanju Baştürk <basturktanju@gmail.com>
2026-02-18 02:09:12 +0300

    Reduce PM2 instances to 1 for landing page

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,8 @@ module.exports = {
     script: 'node_modules/next/dist/bin/next',
     args: 'start',
     cwd: '/var/www/checkly-website',
-    instances: 'max',
-    exec_mode: 'cluster',
+    instances: 1,
+    exec_mode: 'fork',
     env: {
       NODE_ENV: 'production',
       PORT: 3000,
